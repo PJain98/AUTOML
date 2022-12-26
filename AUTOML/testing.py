bbdf86c770858a9bc69692a1e24a5ee04817ab20
PJain98 <68669316+PJain98@users.noreply.github.com>
2022-12-26 23:00:32 +0530

    Delete testing.py

--- AUTOML/testing.py
+++
@@ -1,7 +0,0 @@
-import pickle
-from pycaret.regression import setup, pull, compare_models, save_model,load_model,predict_model
-pipeline=load_model('templates/best_model')
-
-print(pipeline)
-#print(predict_model(pipeline,data=data1.csv))
-
